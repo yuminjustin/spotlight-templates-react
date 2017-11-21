563a997ed41cf8fb730b378f4b7afc81225d8677
justin lee <yuminjustin@163.com>
2017-11-21 16:30:07 +0800

    Update webpack.js

--- template/build/dev/webpack.js
+++ template/build/dev/webpack.js
@@ -12,7 +12,7 @@ module.exports = {
         })
     },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].js',
         chunkFilename: '[name].bundle.js',
         publicPath: config.dev.publicPath,
         path: config.dev.outputPath
