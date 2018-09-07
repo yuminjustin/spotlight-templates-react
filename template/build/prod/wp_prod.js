a684cf65956d9ef7ae941baf321982c848383ca1
justin lee <yuminjustin@163.com>
2018-09-07 15:55:28 +0800

    MiniCssExtractPlugin

--- template/build/prod/wp_prod.js
+++ template/build/prod/wp_prod.js
@@ -3,41 +3,33 @@ var path = require('path')
 var webpack = require('webpack')
 var merge = require('webpack-merge')
 var CopyWebpackPlugin = require('copy-webpack-plugin')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 var utils = require('../common/utils')
 var webpackBase = require("../common/base")
 var config = require('../config')
 
 var _build = config.build,
-    _dir = './static',
     HWP_arr = utils.HtmlWPMaker(_build),
     webpackConfig = {
         mode: 'production',
         module: {
-            rules: utils.styleLoaders({
-                sourceMap: true,
-                extract: true
-            })
+            rules: utils.styleLoaders()
         },
-        output: {
-            filename: path.posix.join(_dir, 'js/[name].[chunkhash].js'),
-            chunkFilename: path.posix.join(_dir, 'js/[name].[chunkhash].js'),
+        output: Object.assign(utils.filenames('js'),{
             path: path.resolve(__dirname, _build.outputPath)
-        },
+        }),
         devtool: '#source-map',
         plugins: HWP_arr.concat([
             new webpack.DefinePlugin({
                 'process.env': _build.env
             }),
-            new ExtractTextPlugin({
-                filename: path.posix.join(_dir, 'css/[name].[chunkhash].css')
-            }),
             new CleanWebpackPlugin(_build.outputPathName, {
                 root: path.resolve(__dirname, '../../')
             }),
-            new OptimizeCSSPlugin({}),
+            new MiniCssExtractPlugin(utils.filenames('css')),
             new CopyWebpackPlugin([{
                 from: _build.static,
                 to: _build.newStatic,
@@ -46,26 +38,41 @@ var _build = config.build,
         ]),
         optimization: { /* 参考 webpack 官方示例配置 特殊要求自行配置*/
             /* https://github.com/webpack/webpack/tree/master/examples */
+            occurrenceOrder: true,
+            runtimeChunk: {
+                name: "manifest"
+            },
             splitChunks: {
                 cacheGroups: {
-                    commons: {
-                        chunks: "initial",
-                        minChunks: 2,
-                        maxInitialRequests: 5, 
-                        minSize: 0 
+                    styles: {
+                        name: 'styles',
+                        test: /\.css$/,
+                        chunks: 'all',
+                        enforce: true
                     },
                     vendor: {
                         test: /node_modules/,
-                        chunks: "initial",
+                        chunks: "all",
                         name: "vendor",
                         priority: 10,
                         enforce: true
+                    },
+                    commons: {
+                        chunks: "initial",
+                        minChunks: 2,
+                        maxInitialRequests: 5,
+                        minSize: 0
                     }
                 }
             },
-            runtimeChunk: {
-                name: "manifest"
-            }
+            minimizer: [
+                new UglifyJsPlugin({
+                    cache: true,
+                    parallel: true,
+                    sourceMap: true
+                }),
+                new OptimizeCSSPlugin({})
+            ]
         }
     }
 
