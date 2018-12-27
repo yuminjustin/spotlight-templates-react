ce84b69d1e1c4d0afbb708ebef36d9d9276bfadb
justin lee <yuminjustin@163.com>
2018-12-27 14:37:27 +0800

    Update app.jsx

--- template/src/components/app.jsx
+++ template/src/components/app.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-
+import style from 'B/assets/css/style.css';
 class Test extends Component {
   state = {
     text: "Hello world! Let's react!"
   };
   render() {
     return (
-      <div className="hw">
-        <p>{this.state.text}</p>
+      <div className={style.hw}>
+        <p className={style.hw_p}>{this.state.text}</p>
         <img src="./static/image/white.png" />
       </div>
     );
